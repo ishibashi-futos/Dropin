474814aa7dbc884883c180d45db2d73b39eb56e0
YuukiIshibashi <robopowerful.v@gmail.com>
2023-01-02 13:09:23 +0900

    バリデーション追加

---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <transition name="modal">
+    <div class="modal_overlay" @click.self="$emit('close')">
+      <div class="modal_wrapper">
+        <div class="modal_container p-10">
+          <slot />
+        </div>
+        <span class="text-xl text-white" @click="$emit('close')">×</span>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  &_overlay {
+    position: fixed;
+    z-index: 102;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, .7);
+    animation: fade .5s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &_wrapper {
+    width: 70%;
+    max-height: calc(100vh - 100px);
+    margin-top: 50px;
+    display: flex;
+    @media screen and (max-width: 767px) {
+      margin-top: 0px;
+      width: 90%;
+    }
+  }
+  &_container {
+    animation: fade-flame .5s;
+    overflow: scroll;
+    width: 100%;
+    background-color: white;
+  }
+}
+</style>
--- src/views/Contact/Form.vue
+++ src/views/Contact/Form.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import Api from '../../api/api.js';
-import {useRouter} from "vue-router" 
+import { useRouter } from "vue-router";
+import Modal from '@/components/Modal.vue'
 const router = useRouter()
 
 const postData = ref({
@@ -10,12 +11,24 @@ const postData = ref({
   message: '',
   type: '',
 });
+let modalShow = ref(false);
+let errorShow = ref(false);
+const isValid = computed(() => {
+  return postData.value.email !== '' && postData.value.name !== '' && postData.value.message !== '';
+})
 postData.value.type = router.currentRoute._value.path.startsWith('/guest_house') ? 'guest_house' : 'drop_in';
 
 function handleSubmit() {
+  if (!isValid.value) return false;
   Api.postContact(postData.value).then((data) => {
   console.log(data);
-  postData.value = {}
+  debugger;
+  if(data.id) {
+    postData.value = {}
+    modalShow.value = true;
+  } else {
+    errorShow.value = true;
+  }
 });
 }
 </script>
@@ -23,11 +36,16 @@ function handleSubmit() {
 <template>
   <div class="section">
     <h2 class="text-center text-xl font-bold py-16">お問い合わせ</h2>
+    <p v-if="errorShow" class="text-md text-red-600 pb-6 font-bold">
+      エラーが発生しました。必須項目を確認して再度送信ボタンを押してください。
+      何度もエラーが発生する場合は、お手数ですがminato.dropin@gmail.comまでメールをお願いします。
+    </p>
     <form>
       <dl>
         <dd>
           <label for="subject">
             <span class="text-md font-bold">Eメールアドレス</span>
+            <span class="text-sm text-red-600 pl-3">※必須</span>
             <span class="text-sm pl-3">(こちらに返信いたします)</span>
           </label>
         </dd>
@@ -36,7 +54,8 @@ function handleSubmit() {
             id="email"
             v-model="postData.email"
             name="email"
-            type="text"
+            type="email"
+            required
             class="form-input w-full px-4 py-3 rounded border-slate-300"
           />
         </dt>
@@ -44,6 +63,7 @@ function handleSubmit() {
       <dl class="mt-10">
         <dd>
           <label for="name" class="text-md font-bold">お名前</label>
+          <span class="text-sm text-red-600 pl-3">※必須</span>
         </dd>
         <dt>
           <input
@@ -51,6 +71,7 @@ function handleSubmit() {
             v-model="postData.name"
             name="name"
             type="text"
+            required
             class="form-input w-full px-4 py-3 rounded border-slate-300"
           />
         </dt>
@@ -58,25 +79,45 @@ function handleSubmit() {
       <dl class="mt-10">
         <dd>
           <label for="message" class="text-md font-bold">お問い合わせ内容</label>
+          <span class="text-sm text-red-600 pl-3">※必須</span>
         </dd>
         <dt>
           <textarea
             id="message"
             v-model="postData.message"
             name="message"
+            requireds
             class="form-textarea w-full px-4 py-3 rounded border-slate-300" />
         </dt>
       </dl>
-      <div class="flex justify-center">
-        <div class="btn submitBtn w-fit bg-teal-600 text-white px-8 py-4 text-xl" @click="handleSubmit">送信する</div>
+      <div class="flex justify-center mt-10">
+        <div class="btn w-fit bg-teal-600 text-white px-8 py-4 text-xl"
+          :class="{'opacity-25' : !isValid}"
+          @click="handleSubmit">
+          送信する
+        </div>
       </div>
-      
-    </form>
+      <p v-if="!isValid" class="text-center text-xs text-slate-500 mt-3">
+        全部入力いただけると押せるようになります！
+      </p>
+  </form>
+    <Modal v-if="modalShow" @close="modalShow = false">
+      <div class="flex flex-col md:flex-row items-center justify-center" >
+        <img src="/src/assets/images/GH/icon/icon_GH_GN_owner.png" class="w-5/12 md:w-3/12"/>
+        <div>
+          <p>お問い合わせありがとうございます。</p>
+          <p>お問い合わせ内容をメールにて送信しました。</p>
+          <p>2~3日中に返信させていただきます。</p>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div class="btn w-fit bg-teal-600 text-white px-6 py-3 text-md"
+          @click="modalShow = false">閉じる</div>
+      </div>
+    </Modal>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.submitBtn {
 
-}
 </style>
